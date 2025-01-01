e0132f2e24f9cc4230c0b057feab74740ded506d
AnnetaDe <anneta.liss@gmail.com>
2025-01-01 22:36:23 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.db.database import start_database
 import os
@@ -12,12 +13,18 @@ load_dotenv()
 
 
 app = FastAPI()
-if os.getenv("DEBUG", "false") == "true" or os.getenv("DEBUG", "false") == "1":
-    app.debug = True
-else:
-    app.debug = False
-# app.add_middleware(CORS, expose_headers=["Authorization"], allow_credentials=True)
-# CORS(app, expose_headers="Authorization", allow_credentials=True)
+debug = os.getenv("DEBUG", "false").lower() in ("true", "1", "t")
+if debug:
+    print("Debug mode is enabled")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["Authorization"],
+)
 
 
 @app.get("/")
