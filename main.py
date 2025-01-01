3d58db5fce44ac8dbc84e9262467c77274dce499
AnnetaDe <anneta.liss@gmail.com>
2025-01-02 00:33:35 +0100

    upd

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ import uvicorn
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("DEBUG", "False").lower() in ("true", "1", "t")
-    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=debug)
+    uvicorn.run("app:app", port=port, reload=debug)
     print(port)
